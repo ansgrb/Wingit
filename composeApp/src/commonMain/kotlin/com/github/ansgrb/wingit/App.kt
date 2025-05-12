91d747bab1bd08deea61ea10d49cfb38b508078f
ansgrb <ansgrb@gmail.com>
2025-05-12 18:42:58 +0300

    Simplify UI and enable edge-to-edge rendering
    Updated iOSApp to ignore safe area for full-screen content. Enhanced Compose UI with a background image, score display, and custom font. Enabled edge-to-edge rendering in Android MainActivity for a more immersive experience.

--- composeApp/src/commonMain/kotlin/com/github/ansgrb/wingit/App.kt
+++ composeApp/src/commonMain/kotlin/com/github/ansgrb/wingit/App.kt
@@ -1,14 +1,60 @@
 package com.github.ansgrb.wingit
 
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.github.ansgrb.wingit.util.ChewyFontFamily
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import wingit.composeapp.generated.resources.Res
+import wingit.composeapp.generated.resources.background
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
+
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ){
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painterResource(Res.drawable.background),
+                contentDescription =  "background",
+                contentScale = ContentScale.Crop
+            )
+        }
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(all = 48.dp)
+        ) {
+            Text(
+                text = "BEST: 0",
+                fontFamily = ChewyFontFamily(),
+                fontWeight = FontWeight.Bold,
+                fontSize = MaterialTheme.typography.headlineLarge.fontSize,
+            )
+            Text(
+                text = "0",
+                fontFamily = ChewyFontFamily(),
+                fontWeight = FontWeight.Bold,
+                fontSize = MaterialTheme.typography.headlineLarge.fontSize,
+            )
+        }
+
 
     }
 }
