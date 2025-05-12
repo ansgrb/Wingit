52105df115663f91b790e7c17485b10e1c244c49
ansgrb <ansgrb@gmail.com>
2025-05-12 22:09:34 +0300

    Refactor endgame UI to use Column for centered alignment.
    Replaced Box with Column in the endgame screen to achieve better vertical and horizontal alignment. This ensures a cleaner and more consistent UI layout when the game ends. Additionally, unnecessary padding was removed for a more streamlined look.

--- composeApp/src/commonMain/kotlin/com/github/ansgrb/wingit/App.kt
+++ composeApp/src/commonMain/kotlin/com/github/ansgrb/wingit/App.kt
@@ -7,12 +7,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
@@ -118,10 +120,11 @@ fun App() {
             )
         }
         if (game.status == GameStatus.OVER) {
-            Box(
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(all = 48.dp)
                     .background(color = Color.Black.copy(alpha = 0.5f))
             ) {
                 Text(
